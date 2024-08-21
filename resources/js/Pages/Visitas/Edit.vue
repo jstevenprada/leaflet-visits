06d93b5671870acf3b6377c613bd06cb90abef66
Jonathan Prada <jstevenprada@gmail.com>
2024-08-21 13:39:52 -0500

    Change the aspect of the link button

--- resources/js/Pages/Visitas/Edit.vue
+++ resources/js/Pages/Visitas/Edit.vue
@@ -36,7 +36,7 @@ const sendForm = () => {
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="flex justify-between">
                         <h2 class="m-8 font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Editar visita</h2>
-                        <Link :href="route('visitas.index')">Lista de visitas</Link>
+                        <Link class="m-8 inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150" :href="route('visitas.index')" as="button" type="button">Lista de visitas</Link>
                     </div>
 
                     <div class="flex justify-center">
--- resources/js/Pages/Visitas/Index.vue
+++ resources/js/Pages/Visitas/Index.vue
@@ -16,9 +16,6 @@ const onDeleteSuccess = (e) => {
     <Head title="Visitas" />
 
     <AuthenticatedLayout>
-        <!-- <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard 2</h2>
-        </template> -->
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
